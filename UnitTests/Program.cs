7443bab3ebe3db29c021fd88dea6e41e114e350e
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-02-01 10:43:14 -0500

    Add project files.

---
+++ UnitTests/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using UnitTests.Tests;
+
+Console.WriteLine("Hello, World!");
+
+WorldsDumbestFunctionTest.WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString();
---
+++ UnitTests/Tests/WorldsDumbestFunctionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Tests
+{
+    public static class WorldsDumbestFunctionTest
+    {
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange - Go get your variables, whatever you need, your classes, your functions
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act - Execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert - Whatever is returned is it what you want?
+                if(result == "PIKACHU!")
+                {
+                    Console.WriteLine("Passed: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnsString");
+                }
+                else
+                {
+                    Console.WriteLine("Failed: WorldsDumbestFunction.ReturnsPikachuIfZero_ReturnsString");
+                }
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine(ex);
+            }        
+        }
+            
+    }
+}
