3c973af45f2500d185633e38128f9f080be33a45
MegMoore <136514941+MegMoore@users.noreply.github.com>
2024-02-02 10:46:45 -0500

    ,

--- NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTest/NetworkServiceTests.cs
@@ -4,6 +4,7 @@ using NetworkUtility.Ping;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,6 +65,24 @@ namespace NetworkUtility.Tests.PingTest
             result.Should().BeBefore(1.January(2030));
             
 
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            //Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+            //Act
+            var result = _pingService.GetPingOptions();
+
+            //Assert WARNING: 'Be' careful
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
         }
     }
 
